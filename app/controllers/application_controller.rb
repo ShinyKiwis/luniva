0e06df2effc7a070bc8f23f48ab27b729db094f7
Hoang Nguyen <nguyenhua.hcm@gmail.com>
2025-05-31 15:09:51 +0700

    Implement login page and setup dashboard Vue page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :authenticate_user!
+  before_action :set_application_pack
+
+  private
+
+  def set_application_pack
+    @application_pack = if !request.path.include?('sign_in')
+                         'entrypoints/dashboard'
+                       end
+  end
 end
---
+++ app/frontend/entrypoints/dashboard.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from '../javascript/dashboard/App.vue';
+
+const app = createApp(App);
+app.mount('#app')
